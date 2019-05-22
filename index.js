e1e1a45fa07935873f80a6c9e010924afd9a115c
Jt Whissel <jtwhissel@gmail.com>
2019-05-22 16:06:47 -0400

    [ENG-873] - added cloneDeepWith function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var nodeUtils = function()
 {
   var _clone = require('./lib/clone.js');
+  this.cloneDeepWith = _clone.cloneDeepWith;
   this.cloneDeep = _clone.cloneDeep;
   this.clone = _clone.clone;
 
