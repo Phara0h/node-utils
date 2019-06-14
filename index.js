00839119563e21f1482db8b4a1c9f2a7a852907c
Jt Whissel <jtwhissel@gmail.com>
2019-06-14 19:03:42 -0400

    [ENG-915] - PG and requests added

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var nodeUtils = function()
   this.cloneDeepWith = _clone.cloneDeepWith;
   this.cloneDeep = _clone.cloneDeep;
   this.clone = _clone.clone;
+  this.PGConnecter = require('./lib/postgres/PGConnecter');
+  this.PGBaseModel = require('./lib/postgres/PGBaseModel');
+  this.PGEncryptModel = require('./lib/postgres/PGEncryptModel');
+  this.request = require('./lib/request');
+
 
   return this;
 }();
