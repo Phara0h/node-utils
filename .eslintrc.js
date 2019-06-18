b48fdbeea0f1da141e5f1bedd174b03d1e43d4c6
Jt Whissel <jtwhissel@gmail.com>
2019-06-18 12:30:04 -0400

    [ENG-915] - Reverted back to garbage eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 var MAX_CHARS = 120;
 var SPACES_PER_TAB = 4;
+var MAX_NESTED_CALLBACKS = 4;
+var MAX_PARAMS = 10;
+var MAX_STATEMENTS = 20;
 
 module.exports = {
 
     // http://eslint.org/docs/rules/
 
     'parserOptions': {
-        'ecmaVersion': 8
+        'ecmaVersion': 8,
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+        },
     },
 
     'env': {
@@ -66,64 +72,19 @@ module.exports = {
 
     'rules': {
 
-        ////////// Possible Errors //////////
-
-        'comma-dangle': [2, 'always-multiline'],
-        'no-cond-assign': 2,
-        'no-constant-condition': 2,
-        'no-control-regex': 2,
-        'no-debugger': 2,
-        'no-dupe-args': 2,
-        'no-dupe-keys': 2,
-        'no-duplicate-case': 2,
-        'no-empty-character-class': 2,
-        'no-extra-parens': 2, // maybe blocks scopes
-        'no-extra-semi': 2,
-        'no-func-assign': 2,
-        'no-invalid-regexp': 2,
-        'no-irregular-whitespace': 2,
-        'no-negated-in-lhs': 2,
-        'no-obj-calls': 2,
-        'no-unreachable': 2,
-        'use-isnan': 2,
-        'no-unexpected-multiline': 2,
-
-        ////////// Best Practices //////////
-
-        'curly': 2,
-        'dot-location': [2, 'property'],
-        'no-alert': 2,
-        'no-caller': 1,
-        'no-eval': 1,
-        'no-multi-spaces': 2,
-
-
-        ////////// Strict Mode //////////
-
-        'strict': [2, 'global'],
-
-        ////////// Variables //////////
-
-        'no-undef': 2,
-        'no-unused-vars': [2, {'vars': 'all', 'args': 'none'}],
-
-
-        ////////// Node.js //////////
-
-        'handle-callback-err': [1, '^.*(e|E)rr'],
 
         ////////// Stylistic Issues //////////
 
         'array-bracket-spacing': [2, 'never'],
         'block-spacing': [2, 'never'],
         'brace-style': [2, '1tbs', {'allowSingleLine': true}],
-        'camelcase': ["error", {properties: 'never'}],
+        'camelcase': 2,
         'comma-spacing': [2, {'before': false, 'after': true}],
         'comma-style': [2, 'last'],
         'computed-property-spacing': [2, 'never'],
         'consistent-this': [2, 'self'],
         'eol-last': 2,
-        'func-style': [2, 'declaration'],
+        'func-names': 0,
         'indent': [2, 4, {'SwitchCase': 1}],
         'key-spacing': [2, {'beforeColon': false, 'afterColon': true}],
         'keyword-spacing': 2,
@@ -132,21 +93,26 @@ module.exports = {
             {'beforeBlockComment': true, 'beforeLineComment': false, 'allowBlockStart': true},
         ],
         'linebreak-style': [2, 'unix'],
-        'new-cap': 2,
+        'max-nested-callbacks': [2, MAX_NESTED_CALLBACKS],
         'new-parens': 2,
         'newline-after-var': [2, 'always'],
         'no-array-constructor': 2,
+        'no-continue': 0,
         'no-inline-comments': 2,
         'no-lonely-if': 2,
         'no-mixed-spaces-and-tabs': 2,
         'no-multiple-empty-lines': [2, {max: 1}],
-        'no-nested-ternary': 2,
         'no-new-object': 2,
         'no-spaced-func': 2,
+        'no-ternary': 0,
         'no-trailing-spaces': 2,
+        'no-underscore-dangle': 0,
+        'no-unneeded-ternary': 0,
         'object-curly-spacing': [2, 'never'],
         'one-var': [2, 'never'],
+        'operator-assignment': 0,
         'operator-linebreak': [2, 'before'],
+        'padded-blocks': 0,
         'quote-props': [2, 'as-needed'],
         'quotes': [2, 'single', 'avoid-escape'],
         'semi-spacing': 2,
@@ -162,6 +128,16 @@ module.exports = {
         'space-infix-ops': 2,
         'space-unary-ops': [2, {'words': true, 'nonwords': false}],
         'spaced-comment': [2, 'always', {'block': {'exceptions': ['-+*']} }],
+        'wrap-regex': 0,
+
+        ////////// ECMAScript 6 //////////
+
+        'constructor-super': 0,
+        'generator-star-spacing': 0,
+        'no-this-before-super': 0,
+        'no-var': 0,
+        'object-shorthand': 0,
+        'prefer-const': 0,
 
         ////////// Security //////////
 
@@ -169,9 +145,6 @@ module.exports = {
         'security/detect-non-literal-require': 'off',
         'security/detect-object-injection': 'off',
 
-        ////////// Legacy //////////
 
-        'max-depth': 2,
-        'max-len': [2, MAX_CHARS, SPACES_PER_TAB]
     },
 };
