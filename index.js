b96afc7623965d99c393bb8ec9e34a1e31b5307e
Jt Whissel <jtwhissel@gmail.com>
2019-07-03 15:13:48 -0400

    [ENG-961] - Added timer class and added querystring to request lib

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var nodeUtils = function() {
     this.cloneDeepWith = _clone.cloneDeepWith;
     this.cloneDeep = _clone.cloneDeep;
     this.clone = _clone.clone;
+
     this.PGConnecter = require('./lib/postgres/PGConnecter');
     this.PGBaseModel = require('./lib/postgres/PGBaseModel');
     this.PGEncryptModel = require('./lib/postgres/PGEncryptModel');
+
     this.request = require('./lib/request');
+
     this.keysToCamel = require('./lib/camelCase');
+
+    this.Timer = require('./lib/timer');
 
     return this;
 }();
